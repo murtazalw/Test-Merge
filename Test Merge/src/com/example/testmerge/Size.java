5fb46692a01d09135737182d3dd46f64cb303c65
murtazalw <murtaza_lw@yahoo.com>
2016-06-30 11:10:58 +0300

    medium

--- Test Merge/src/com/example/testmerge/Size.java
+++ Test Merge/src/com/example/testmerge/Size.java
@@ -3,6 +3,7 @@ package com.example.testmerge;
 public class Size {
 	public void getSize() {
 		System.out.println("big");
-		System.out.println("small");
+		System.out.println("small");|
+		System.out.println("medium");|
 	}
 }
