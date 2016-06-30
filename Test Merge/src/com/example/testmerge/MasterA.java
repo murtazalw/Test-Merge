b268e98f0ed3c3a95e1f7fcd218cc89920bf05ef
murtazalw <murtaza_lw@yahoo.com>
2016-06-30 10:58:55 +0300

    blank

--- Test Merge/src/com/example/testmerge/MasterA.java
+++ Test Merge/src/com/example/testmerge/MasterA.java
@@ -5,11 +5,6 @@ public class MasterA {
 	int j;
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("small master");
-=======
-		System.out.println("big master 1");
-		new Size().getSize();
->>>>>>> refs/heads/bigMaster
+
 	}
 }
