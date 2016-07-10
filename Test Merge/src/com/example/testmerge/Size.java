39d8bbe5bad583e40a807de8e849763d31dfbcba
murtazalw <murtaza_lw@yahoo.com>
2016-07-10 09:38:58 +0300

    alphabets

--- Test Merge/src/com/example/testmerge/Size.java
+++ Test Merge/src/com/example/testmerge/Size.java
@@ -2,9 +2,9 @@ package com.example.testmerge;
 
 public class Size {
 	public void getSize() {
-		System.out.println("big");
-		System.out.println("small");
-		System.out.println("medium");
-		System.out.println("Large");
+		System.out.println("A");
+		System.out.println("B");
+		System.out.println("C");
+		System.out.println("D");
 	}
 }
